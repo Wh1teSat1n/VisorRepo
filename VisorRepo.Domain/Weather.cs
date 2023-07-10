bf2bbc40e25d77fc6f08a72dea22dd6f8c81fb8f
Egor Sazanov <egorsazanov97@gmail.com>
2023-07-10 14:38:49 +0300

    project init

---
+++ VisorRepo.Domain/Weather.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace VisorRepo.Domain
+{
+    public class Weather
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public float Temperature { get; set; }
+        public string Condition { get; set; }
+        public int Humidity { get; set; }        
+        public string WindDirection { get; set; }
+        public int WindSpeed { get; set; }
+    }
+}
